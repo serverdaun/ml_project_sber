1f5e75e5e35a31be2f2aaed970e6a1e07ea1cc60
Vasilii Tokarev <notsoyunng@gmail.com>
2024-06-22 23:49:36 +0200

    add weighted model with better performance

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, roc_auc_score, roc_curve
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from category_encoders import TargetEncoder
@@ -67,3 +70,46 @@ class PreprocessingUtils:
         df = df.drop(columns=[column], axis=1)
 
         return df
+
+    @staticmethod
+    def evaluate_model(y_true, y_pred, y_pred_proba):
+        result = {}
+
+        result['accuracy'] = accuracy_score(y_true, y_pred)
+
+        result['f1_score'] = f1_score(y_true, y_pred)
+
+        cm = confusion_matrix(y_true, y_pred)
+        result['confusion_matrix'] = cm
+
+        result['roc_auc'] = roc_auc_score(y_true, y_pred_proba)
+
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+        plt.title('Confusion Matrix')
+        plt.ylabel('True Label')
+        plt.xlabel('Predicted Label')
+        plt.show()
+
+        print(f"Accuracy: {result['accuracy']:.2f}")
+        print(f"F1 Score: {result['f1_score']:.2f}")
+        print(f"ROC-AUC Score: {result['roc_auc']:.2f}")
+
+        return result
+
+    @staticmethod
+    def create_roc_auc_curve(roc_auc, y_test, y_pred_proba):
+        fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
+        plt.figure(figsize=(10, 6))
+        plt.plot(fpr, tpr, label=f'ROC curve (area = {roc_auc:.2f})')
+        plt.plot([0, 1], [0, 1], 'k--', lw=2)
+
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic (ROC) Curve')
+        plt.legend(loc="lower right")
+
+        plt.show()
